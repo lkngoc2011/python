148b647c3917de62f72fff9d1a4a8cf847e876ce
lkngoc2011 <46101266+lkngoc2011@users.noreply.github.com>
2018-12-24 17:56:00 +0700

    Add hair color

--- ex12.py
+++ ex12.py
@@ -1,6 +1,6 @@
 age = input("How old are you? ")
 height = input("How tall are you? ")
 weight = input("How much do you weight? ")
-
+hair_color = input("What is your hair color?")
 print("So, you're %r old, %r tall and %r weight"
       % (age, height, weight))
