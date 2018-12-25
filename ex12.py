b8fd04f52f38a4276b55c33349828fb52889f412
Ngoc Le <lkngoc2011@gmail.com>
2018-12-25 10:03:07 +0700

    Add test messsage

--- ex12.py
+++ ex12.py
@@ -4,3 +4,4 @@ weight = input("How much do you weight? ")
 hair_color = input("What is your hair color?")
 print("So, you're %r old, %r tall and %r weight"
       % (age, height, weight))
+print("This is test message")
