6dc22216fa124d8fa86fe81c8d4ee5c8bd00e6c2
lkngoc2011 <46101266+lkngoc2011@users.noreply.github.com>
2018-12-25 09:58:14 +0700

    revert the fix

--- ex12.py
+++ ex12.py
@@ -4,4 +4,3 @@ weight = input("How much do you weight? ")
 hair_color = input("What is your hair color?")
 print("So, you're %r old, %r tall and %r weight"
       % (age, height, weight))
-print("So, your hair color is %r", %(hair_color))
