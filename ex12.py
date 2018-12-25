3c5a16e8f6fc274e6aa9789e4c748d17a0a7ddf4
Ngoc Le <lkngoc2011@gmail.com>
2018-12-25 10:09:07 +0700

    Update new file

--- ex12.py
+++ ex12.py
@@ -1,7 +1,11 @@
-age = input("How old are you? ")
-height = input("How tall are you? ")
-weight = input("How much do you weight? ")
-hair_color = input("What is your hair color?")
-print("So, you're %r old, %r tall and %r weight"
-      % (age, height, weight))
-print("This is test message")
+def add(x,y):
+	pass
+	
+def subtract(x,y):
+	pass
+
+def multiply(x,y):
+	pass
+
+def divide(x,y):
+	pass:
